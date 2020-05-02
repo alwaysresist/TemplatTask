be727f2763b49e013833654a086b7b8c44cdc34a
alwaysresist <44613719+alwaysresist@users.noreply.github.com>
2020-05-02 18:42:34 +0300

    1

---
+++ Task2c/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLtree<int> tree = new AVLtree<int>();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            tree.Insert(4);
+            tree.Insert(5);
+            tree.Remove(4);
+            bool search1 = tree.Find(1);
+            bool search2 = tree.Find(4);
+
+            Console.Write("Существует элемент 1:" + search1.ToString());
+            Console.WriteLine();
+            Console.Write("Существует элемент 4: " + search2.ToString());
+            Console.WriteLine();
+
+            int[] arr = tree.Leaves();
+            for (int i = 0; i < tree.getCount(); i++)
+                Console.Write("{0} ", arr[i]);
+            Console.WriteLine();
+        }
+    }
+}
